d2e75da8397fd0afe47c8aba548dab41dece6a13
kilbergr <rekilberg@gmail.com>
2015-08-18 22:59:25 -0700

    past cors issue

--- 4_Ch_httpService/chatApp/controllers.js
+++ 4_Ch_httpService/chatApp/controllers.js
@@ -1,18 +1,26 @@
-app.controller('ChatController', function($scope, $http){
-	
-	
-	$scope.setMessage = function(){
+
+app.controller("ChatController", function($scope, $http){
+              
+
+$scope.setMessage = function(){
 		$scope.newMessage = {
 			name: "",
 			content: ""
-		};
+		}
+	};
+$scope.setMessage();	
+
+$scope.getMessages = function(){
+	$http.get('https://shielded-peak-6345.herokuapp.com/')
+		.then(function(data){
+			$scope.messages = data.data
+			});
 	}
-
-	$scope.setMessage();
-
-	$scope.addMessage = function(){
+	$scope.getMessages();
+	        
+$scope.addMessage = function(){
 		if ($scope.newMessage.name && $scope.newMessage.content){
-			$http.post('http://shielded-peak-6345.herokuapp.com/', $scope.newMessage)
+			$http.post('https://shielded-peak-6345.herokuapp.com/messages', $scope.newMessage)
 			.then(function(data){
 				$scope.data = data
 			})
@@ -21,12 +29,4 @@ app.controller('ChatController', function($scope, $http){
 		$scope.setMessage();
 	}
 
-$scope.getMessages = function(){
-	$http.get('http://shielded-peak-6345.herokuapp.com/')
-	.then(function(data){
-		$scope.messages = data.data
-		})
-	}
-
-
-})
+});
