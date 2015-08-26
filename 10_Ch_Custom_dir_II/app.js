ec5ad463608352ceab7173c422d6401607f36672
kilbergr <rekilberg@gmail.com>
2015-08-26 12:13:15 -0700

    custom directive with link to change css

--- 10_Ch_Custom_dir_II/app.js
+++ 10_Ch_Custom_dir_II/app.js
@@ -2,21 +2,26 @@ var app = angular.module('mouseOverDirectiveApp', [])
 
 app.directive('gsChangeBackground', function() {
   return {
+    restrict: 'E',
     link: function(scope, element, attrs) {
 
       var oldColor = element.css('background-color');
 
       element.on('mouseenter', function(event) {
-        element.css('background-color', 'yellow');
+        element.css('background-color', 'background');
       });
 
-      element.on('mouseleave', function(event) {
-        element.css('background-color', oldColor);
-      })
-
-      element.on('keydown', function(event){
-        element.css('background-color', 'yellow')
+       element.on('mouseenter', function(event) {
+        element.css('color', 'color');
       });
+
+      // element.on('mouseleave', function(event) {
+      //   element.css('background-color', oldColor);
+      // })
+
+      // element.on('click', function(event){
+      //   element.css('background-color', 'green')
+      // });
     }
   };
 });
